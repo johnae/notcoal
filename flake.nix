bd9add63302f9b950b95d7dac4d16a9f60761da6
John Axel Eriksson <john@insane.se>
2020-08-21 08:14:33 +0200

    Fix nix build

--- flake.nix
+++ flake.nix
@@ -9,6 +9,26 @@
   outputs = { self, flake-utils, ... }@inputs:
     let
       genAttrs' = values: f: builtins.listToAttrs (map f values);
+      package = pkgs: {
+        pname = "notcoal";
+        version = "v0.3.0";
+        src = self;
+        cargoSha256 = "sha256-JjwW/9JlsGPfsInZlLiVCHLKkzVIorEUDV5RLLmE5hY=";
+        doCheck = false;
+        cargoBuildFlags = [ "--features standalone" ];
+        nativeBuildInputs = [ pkgs.pkgconfig ];
+        buildInputs = [ pkgs.notmuch ];
+        meta = {
+          license = pkgs.stdenv.lib.licenses.mit;
+          maintainers = [
+            {
+              email = "john@insane.se";
+              github = "johnae";
+              name = "John Axel Eriksson";
+            }
+          ];
+        };
+      };
     in
     flake-utils.lib.eachDefaultSystem
       (system:
@@ -22,28 +42,13 @@
         in
         {
 
+          defaultPackage = nixpkgs.rustPlatform.buildRustPackage (package nixpkgs);
           devShell = import ./shell.nix { inherit nixpkgs; };
 
         }
       ) // {
       overlay = final: prev: {
-        notcoal = prev.rustPlatform.buildRustPackage {
-          pname = "notcoal";
-          version = "v0.3.0";
-          src = self;
-          cargoSha256 = "sha256-OhVg5r24Zfjr2bY3dkRg6rztFQB9oPlp6LB04ETbElw=";
-          doCheck = false;
-          meta = {
-            license = prev.stdenv.lib.licenses.mit;
-            maintainers = [
-              {
-                email = "john@insane.se";
-                github = "johnae";
-                name = "John Axel Eriksson";
-              }
-            ];
-          };
-        };
+        notcoal = prev.rustPlatform.buildRustPackage (package inputs.nixpkgs);
       };
 
     };
